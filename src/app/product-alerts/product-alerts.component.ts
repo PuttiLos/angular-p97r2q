5f7384c57c8a463223e80a2a40a21ed928e0c16a
PuttiLos <puttimait@hotmail.com>
2022-08-01 13:56:29 +0700

    4 files modified : child to parent

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {Product} from '../products';
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 
 }
